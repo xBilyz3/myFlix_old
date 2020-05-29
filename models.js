bdd3cb3c2972e2d4d3666dfe640f01be9190af93
Konrad Bil <konrad_bil@t-online.de>
2020-05-29 19:02:14 +0530

    added hashPassword fuction

--- models.js
+++ models.js
@@ -1,31 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 var movieSchema = mongoose.Schema({
-    Title : {type: String, required: true},
-    Description : {type: String, required: true},
-    Genre : {
-        Name : String,
-        Description : String,
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    Genre: {
+        Name: String,
+        Description: String,
     },
-    Director : {
-        Name : String,
-        Bio : String
+    Director: {
+        Name: String,
+        Bio: String,
     },
-    Actors : [String],
-    ImagePath : String,
-    Featured : Boolean
-});
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean,
+})
 
 var userSchema = mongoose.Schema({
-    Username : {type: String, required: true},
-    Password : {type: String, required: true},
-    Email : {type: String, required: true},
-    Birthday : Date,
-    FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
-  });
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true },
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movies' }],
+})
 
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
+userSchema.statics.hashPassword = function (password) {
+    return bcrypt.hashSync(password, 10)
+}
 
-module.exports.Movie = Movie;
-module.exports.User = User;
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.Password)
+}
+
+let Movie = mongoose.model('Movie', movieSchema)
+let User = mongoose.model('User', userSchema)
+
+module.exports.Movie = Movie
+module.exports.User = User

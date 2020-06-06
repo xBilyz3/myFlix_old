a14cb98dd1af8bee0144b77c8e235a0d276a1e5a
Konrad Bil <konrad_bil@t-online.de>
2020-06-06 16:56:31 +0530

    added basic structure to set up React Frontend

---
+++ client/src/components/movie-view/movie-view.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: null,
+      selectedMovie: null,
+    };
+  }
+
+  handleClick(movie) {
+    console.log('this is:', this);
+  }
+
+  render() {
+    const { movie, onClick } = this.props;
+
+    if (!movie) return null;
+
+    return (
+      <div className="movie-view">
+        <img className="movie-poster" src={movie.ImagePath} />
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div>
+          <button onClick={() => onClick(null)}>Back</button>
+        </div>
+      </div>
+    );
+  }
+}

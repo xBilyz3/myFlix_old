9d2f5eff00b833971efdc290466b9f7020313ec1
Konrad Bil <konrad_bil@t-online.de>
2020-06-15 18:43:04 +0530

    added react bootstrap, scss for myFlix UI

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import './movie-card.scss';
+
+// get bootstrap imports
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class MovieCard extends React.Component {
 	render() {
 		// This is given to the <MovieCard/> component by the outer world
 		// which, in this case, is `MainView`, as `MainView` is what’s
 		// connected to your database via the movies endpoint of your API
-		const { movie, onClick } = this.props;
+		const { movie, toMovieView } = this.props;
 
 		return (
-			<div onClick={() => onClick(movie)} className="movie-card">
-				{movie.Title}
-			</div>
+			<Col xs={true} sm={true} md={true} lg={true} xl={2}>
+				<Card onClick={() => toMovieView(movie)}>
+					<div className="overlay" />
+					<Card.Img variant="top" src={movie.ImagePath} />
+					<Card.Body onClick={() => toMovieView(movie)}>
+						<Card.Title>{movie.Title}</Card.Title>
+						{/*<Card.Text>{movie.Description}</Card.Text>*/}
+					</Card.Body>
+				</Card>
+			</Col>
 		);
 	}
 }
+
+/* static propTypes on MovieCard set to object 
+that contains special values provided as utilities by prop-types module.
+This values help specify how MovieCard's props should look*/
+MovieCard.propTypes = {
+	movie: PropTypes.shape({
+		Title: PropTypes.string,
+		ImagePath: PropTypes.string,
+	}).isRequired,
+};
--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './movie-view.scss';
+
+// get bootstrap imports
+import Button from 'react-bootstrap/Button';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class MovieView extends React.Component {
   constructor() {
@@ -15,33 +25,66 @@ export class MovieView extends React.Component {
   }
 
   render() {
-    const { movie, onClick } = this.props;
+    const { movie, toMovieCard } = this.props;
 
     if (!movie) return null;
 
     return (
       <div className="movie-view">
-        <img className="movie-poster" src={movie.ImagePath} />
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div>
-          <button onClick={() => onClick(null)}>Back</button>
-        </div>
+        <Container>
+          <Row>
+            <Col>
+              <img className="movie-poster" src={movie.ImagePath} />
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col>
+              <div className="movie-view-header">
+                <div className="movie-title">
+                  <span className="value">{movie.Title}</span>
+                </div>
+                <div className="movie-genre">
+                  <span className="label">Genre: </span>
+                  <span className="value">{movie.Genre.Name}</span>
+                </div>
+                <div className="movie-director">
+                  <span className="label">Director: </span>
+                  <span className="value">{movie.Director.Name}</span>
+                </div>
+              </div>
+              <div className="movie-description">
+                <span className="label">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </div>
+              <Button
+                className="back-button"
+                variant="dark"
+                onClick={() => toMovieCard(null)}>
+                back
+              </Button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string,
+    Title: PropTypes.string,
+    ImagePath: PropTypes.string,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string,
+    }),
+  }).isRequired,
+};
